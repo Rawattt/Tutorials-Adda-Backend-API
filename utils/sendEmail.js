8995ca95e69fa3dd044bf1dd64599da77ff7c6b4
Rawattt <rawatrajput7@gmail.com>
2020-08-10 00:52:33 +0530

    Initial

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -22,8 +22,6 @@ const sendEmail = async (options) => {
   };
 
   const info = await transporter.sendMail(message);
-
-  console.log("Message sent: %s", info.messageId);
 };
 
 module.exports = sendEmail;
